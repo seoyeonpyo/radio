f981b38d64aa68bcf06fd868db8e45817e77ea9a
seoyeonpyo <117896485+seoyeonpyo@users.noreply.github.com>
2022-12-04 15:39:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("lightsON")
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("colorON")
+})
 radio.setGroup(77)
 radio.setTransmitPower(7)
 basic.forever(function () {
